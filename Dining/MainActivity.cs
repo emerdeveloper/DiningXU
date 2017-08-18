a360ff8972493f625cb0d071aac6b195f7157c71
Emerson Javid Gonzalez Morales <1388310@SOAM.TCS.com>
2017-08-18 09:52:29 -0500

    Exercise 1: Create a RecyclerView. This exercise does setup: create a new project and add a RecyclerView to your main Activity.

---
+++ Dining/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+
+namespace Dining
+{
+    [Activity(Label = "Dining", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+        }
+    }
+}
+
